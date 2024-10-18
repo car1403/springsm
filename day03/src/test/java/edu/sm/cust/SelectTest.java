8800e44c35262b05bda93a6f7ca6a790dbb897da
jmlee <jmlee@tonesol.com>
2024-10-18 11:06:55 +0900

    1018

--- day03/src/test/java/edu/sm/cust/SelectTest.java
+++ day03/src/test/java/edu/sm/cust/SelectTest.java
@@ -2,6 +2,7 @@ package edu.sm.cust;
 
 import edu.sm.app.dto.CustDto;
 import edu.sm.app.service.CustService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,7 @@ class SelectTest {
     @Test
     void contextLoads() {
         try {
-            List<CustDto> list = custService.get();
-            log.info(list.toString());
+            custService.get();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
